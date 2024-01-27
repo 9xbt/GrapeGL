51ebd307bf079606e8a71c073e4514f322ee34ed
ekeleze <ekeleze@ekeleze.net>
2024-01-27 12:08:16 -0600

    undo broken changes to font

--- src/GrapeGL/Graphics/Fonts/Font.cs
+++ src/GrapeGL/Graphics/Fonts/Font.cs
@@ -35,7 +35,7 @@ public class Font
 	/// </summary>
 	/// <param name="Text">String to measure.</param>
 	/// <returns>Width of the input string.</returns>
-	public ushort MeasureString(string Text, int SpacingModifier = 0)
+	public ushort MeasureString(string Text)
 	{
 		ushort Width = 0;
 
@@ -54,7 +54,7 @@ public class Font
 					Width += (ushort)(Size * 4);
 					continue;
 				default:
-					Width += (ushort)(GetGlyph(Text[I]).Width + 2 - (I * -SpacingModifier));
+					Width += (ushort)(GetGlyph(Text[I]).Width + 2);
 					continue;
 			}
 		}
